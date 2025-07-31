85aad6186fade647cb9cea55c832321b98706ed7
Jordan Yeh <jordanyeh2003@gmail.com>
2025-07-31 14:02:16 -0400

    Added shader loading method and put shader files into their own glsl file

---
+++ App/Shaders/vertex.glsl
@@ -0,0 +1,17 @@
+#version 330 core
+        layout (location = 0) in vec3 aPos;
+        layout (location = 1) in vec3 aColor;
+        layout (location = 2) in vec3 aNormal;
+        
+        out vec3 vertexColor;
+        out vec3 vertexNormal;
+        uniform mat4 world;
+        uniform mat4 view;
+        uniform mat4 projection;
+        
+        void main()
+        {
+           vertexNormal = aNormal;
+           vertexColor = aColor;
+           gl_Position = projection * view * world * vec4(aPos, 1.0);
+        };
