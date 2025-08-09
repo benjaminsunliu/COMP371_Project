ee52207a03ab6534667700b376abf1ab082dedd5
Jordan Yeh <jordanyeh2003@gmail.com>
2025-08-08 23:35:14 -0400

    reverting my last push

---
+++ App/Shaders/fragment.glsl
@@ -0,0 +1,8 @@
+#version 330 core
+        in vec3 vertexColor;
+        in vec3 vertexNormal;
+        out vec4 FragColor;
+        void main()
+        {
+           FragColor = vec4(0.5f*vertexNormal+vec3(0.5f), 1.0f);
+        }
--- App/Shaders/textureFragment.glsl
+++ App/Shaders/textureFragment.glsl
@@ -1,36 +1,16 @@
 #version 330 core
-
-        
-        out vec4 FragColor;
-        
         in vec3 vertexColor;
         in vec2 vertexUV;
-        in vec3 vertexNormal;
-        in vec3 crntPos;
-
         uniform sampler2D textureSampler;
-        uniform vec4 lightColor;
-        uniform vec3 lightPos;
-        uniform vec3 camPos;
-        
+        uniform bool useBlackKey;
+        out vec4 FragColor;
         void main()
         {
-            // ambient lighting
-            float ambient = 0.20f;
-
-            // diffuse lighting
-            vec3 normal = normalize(vertexNormal);
-            vec3 lightDirection = normalize(lightPos - crntPos);
-            float diffuse = max(dot(normal, lightDirection), 0.0f);
-
-            // spec lighting
-            float specularLight = 0.50f;
-            vec3 viewDirection = normalize(camPos - crntPos);
-            vec3 reflectDirection = reflect(-lightDirection, normal);
-            float specAmount = pow(max(dot(viewDirection, reflectDirection), 0.0f), 8);
-            float specular = specAmount * specularLight;
-
-            // final color output
-            FragColor = texture(textureSampler, vertexUV) * lightColor * (diffuse + ambient + specular);
-            
+            vec4 tex = texture(textureSampler, vertexUV);
+            if (useBlackKey) {
+                if (tex.r < 0.05 && tex.g < 0.05 && tex.b < 0.05) discard;
+                FragColor = vec4(tex.rgb, 1.0);
+            } else {
+                FragColor = tex;
+            }
         }
